faa6464ef081641b390537121189453b0a367061
Roberto_Tello <Roberto.Tello@itksquare.edu.mx>
2022-10-18 01:11:44 -0500

    drarft2

--- practice6.dart
+++ practice6.dart
@@ -1,2 +1,7 @@
 //hasEqualChar(“king”, “raja”) // true
 //hasEqualChar(“queen”, “rani”) //false
+class Practice6{
+  bool hasEqualChar(String one, String two ){
+return(one.length == two.length ?true:false);
+  }
+}
