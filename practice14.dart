5ae5f018c3c1be4d29357871760f0011aadc873f
Roberto_Tello <Roberto.Tello@itksquare.edu.mx>
2022-10-19 00:46:07 -0500

    draft4

--- practice14.dart
+++ practice14.dart
@@ -1,3 +1,14 @@
 /*filterData(input: [200, 500, 222, 30, 999], value: 500) // [200, 222, 30]
 filterData(input: [199, 500, 222, 30, 999], value: 200) // [199,30]
 */
+class Practice14{
+  List filterData({required List input, int value=0 }){
+    List data =[]; 
+    for (var i = 0; i < input.length; i++) {
+      if(input[i]<value){
+        data.add(input[i]);
+      }
+    }
+    return data;
+  }
+}
--- practice16.dart
+++ practice16.dart
@@ -2,3 +2,15 @@
 getCharPosition(“mongoose”, “o”) // 2
 getCharPosition(“mongoose”, “k”) // null
 */
+class Practice16{
+  getCharPosition(String str, String focus){
+    var index = null;
+    for (var i = 0; i < str.length; i++) {
+      if(str[i]==focus){
+        index = i++;
+        break;
+      }
+    }
+    return index;
+  }
+}
